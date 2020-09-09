1297bcfd5b0a60bae1f678e1d2f47be0b4ed4059
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-09-08 21:42:22 -0400

    added background color for first destination

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+const destinationEvent = document.querySelectorAll(".destination");
+
+destinationEvent[0].addEventListener('mouseover',()=>{
+    destinationEvent[0].style.backgroundColor = "blue";
+})
+
+
+
