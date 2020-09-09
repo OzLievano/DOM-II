70909894af5f25c11101e37d1f1d9e7c1b6d443b
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-09-08 22:12:08 -0400

    added in resizing event, double click event, and click event

--- js/index.js
+++ js/index.js
@@ -2,10 +2,39 @@
 
 
 const destinationEvent = document.querySelectorAll(".destination");
-
+//1
 destinationEvent[0].addEventListener('mouseover',()=>{
     destinationEvent[0].style.backgroundColor = "blue";
 })
 
+//2 
 
+destinationEvent[0].addEventListener('mouseout',()=>{
+    destinationEvent[0].style.backgroundColor = "white";
+})
+//3
+const clickLogo = document.querySelector(".logo-heading");
+
+clickLogo.addEventListener('click',()=>{
+    clickLogo.style.backgroundColor = 'red';
+})
+
+//4 
+
+const navs = document.querySelectorAll(".nav");
+
+navs.forEach(nav => {
+    nav.addEventListener('dblclick',(event) => {
+        event.target.style.color = "blue";
+    })
+})
+
+//5 
+
+const smallScreen = document.querySelector(".intro img");
+
+//6 
+window.addEventListener("resize",() => {
+   smallScreen.src = "";
+})
 
